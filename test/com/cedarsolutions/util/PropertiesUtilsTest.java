46e7158f548f8493019a8c94be319a4644f89805
Kenneth J. Pronovici <pronovic@ieee.org>
2014-03-10 14:20:45 -0500

    Add PropertiesUtils.getMatchingPrefixes() and PropertiesUtils.getMatchingChildren().

--- test/com/cedarsolutions/util/PropertiesUtilsTest.java
+++ test/com/cedarsolutions/util/PropertiesUtilsTest.java
@@ -23,7 +23,9 @@
 package com.cedarsolutions.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
@@ -92,6 +94,140 @@ public class PropertiesUtilsTest {
         } finally {
             FilesystemUtils.removeFile("build/tmp/PropertiesUtilsTest/invalid.properties");
         }
+    }
+
+    /** Test getMatchingPrefixes() for null properties. */
+    public void testGetMatchingPrefixesNull() {
+        Properties properties = null;
+        List<String> matches = PropertiesUtils.getMatchingPrefixes(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for empty properties. */
+    public void testGetMatchingPrefixesEmpty() {
+        Properties properties = new Properties();
+        List<String> matches = PropertiesUtils.getMatchingPrefixes(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for properties where nothing matches the prefix. */
+    public void testGetMatchingPrefixesNoMatch() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingPrefixes(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for properties where non-parent matches the prefix. */
+    public void testGetMatchingPrefixesNonParent() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingPrefixes(properties, "b");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for properties where a single parent matches the prefix. */
+    public void testGetMatchingPrefixesSingleParent() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingPrefixes(properties, "c");
+        assertFalse(matches.isEmpty());
+        assertEquals(1, matches.size());
+        assertEquals("c.one", matches.get(0));
+    }
+
+    /** Test getMatchingPrefixes() for properties where a multiple parents match the prefix. */
+    public void testGetMatchingPrefixesMultipleParents() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingPrefixes(properties, "a");
+        assertFalse(matches.isEmpty());
+        assertEquals(2, matches.size());
+        assertTrue(matches.contains("a.two"));
+        assertTrue(matches.contains("a.three"));
+    }
+
+    /** Test getMatchingPrefixes() for properties where parents and non-parents match the prefix. */
+    public void testGetMatchingPrefixesVarious() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingPrefixes(properties, "starts.with.d");
+        assertFalse(matches.isEmpty());
+        assertEquals(2, matches.size());
+        assertTrue(matches.contains("starts.with.d.two"));
+        assertTrue(matches.contains("starts.with.d.three"));
+    }
+
+    /** Test getMatchingChildren() for null properties. */
+    public void testGetMatchingChildrenNull() {
+        Properties properties = null;
+        List<String> matches = PropertiesUtils.getMatchingChildren(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingChildren() for empty properties. */
+    public void testGetMatchingChildrenEmpty() {
+        Properties properties = new Properties();
+        List<String> matches = PropertiesUtils.getMatchingChildren(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingChildren() for properties where nothing matches the prefix. */
+    public void testGetMatchingChildrenNoMatch() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingChildren(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingChildren() for properties where non-parent matches the prefix. */
+    public void testGetMatchingChildrenNonParent() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingChildren(properties, "b");
+        assertFalse(matches.isEmpty());
+        assertEquals(1, matches.size());
+        assertEquals("b.one", matches.get(0));
+    }
+
+    /** Test getMatchingChildren() for properties where a single parent matches the prefix. */
+    public void testGetMatchingChildrenSingleParent() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingChildren(properties, "c");
+        assertFalse(matches.isEmpty());
+        assertEquals(1, matches.size());
+        assertEquals("c.one", matches.get(0));
+    }
+
+    /** Test getMatchingChildren() for properties where a multiple parents match the prefix. */
+    public void testGetMatchingChildrenMultipleParents() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingChildren(properties, "a");
+        assertFalse(matches.isEmpty());
+        assertEquals(2, matches.size());
+        assertTrue(matches.contains("a.two"));
+        assertTrue(matches.contains("a.three"));
+    }
+
+    /** Test getMatchingChildren() for properties where parents and non-parents match the prefix. */
+    public void testGetMatchingChildrenVarious() {
+        Properties properties = createProperties();
+        List<String> matches = PropertiesUtils.getMatchingChildren(properties, "starts.with.d");
+        assertFalse(matches.isEmpty());
+        assertEquals(3, matches.size());
+        assertTrue(matches.contains("starts.with.d.one"));
+        assertTrue(matches.contains("starts.with.d.two"));
+        assertTrue(matches.contains("starts.with.d.three"));
+    }
+
+    /** Create properties for testing. */
+    private static Properties createProperties() {
+        Properties properties = new Properties();
+
+        properties.put("a.two.1", "a21");
+        properties.put("a.two.2", "a21");
+        properties.put("a.three.1", "a31");
+        properties.put("b.one", "b1");
+        properties.put("c.one.1", "c11");
+        properties.put("starts.with.d.one", "d1");
+        properties.put("starts.with.d.two.1", "d21");
+        properties.put("starts.with.d.two.2", "d21");
+        properties.put("starts.with.d.three.1", "d31");
+
+        return properties;
     }
 
 }
--- util/com/cedarsolutions/util/PropertiesUtils.java
+++ util/com/cedarsolutions/util/PropertiesUtils.java
@@ -22,7 +22,11 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package com.cedarsolutions.util;
 
+import static com.cedarsolutions.util.StringUtils.substring;
+
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import com.cedarsolutions.exception.InvalidDataException;
@@ -59,6 +63,73 @@ public class PropertiesUtils {
         }
 
         return properties;
+    }
+
+    /**
+     * Get a list of existing prefixes which match the passed-in prefix one level down.
+     *
+     * <p>
+     * For instance, if prefix is "database" and the properties object contains properties
+     * like "database.production.x.y" and "database.integration.x.y.", the returned list
+     * will contain "database.production" and "database.integration".
+     * </p>
+     *
+     * @param properties   Properties to look in
+     * @param parentPrefix Parent property prefix to use as starting point
+     *
+     * @return List of prefixes that match the passed-in prefix, always non-null.
+     */
+    public static List<String> getMatchingPrefixes(Properties properties, String parentPrefix) {
+        List<String> matches = new ArrayList<String>();
+
+        if (properties != null) {
+            for (Object key : properties.keySet()) {
+                String name = (String) key;
+                if (name.startsWith(parentPrefix + ".")) {
+                    String remaining = substring(name, (parentPrefix + ".").length());
+                    if (remaining.indexOf(".") != -1) {
+                        String prefix = parentPrefix + "." + substring(remaining, 0, remaining.indexOf("."));
+                        if (!matches.contains(prefix)) {
+                            matches.add(prefix);
+                        }
+                    }
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    /**
+     * Get a list of children which have a given parent prefix.
+     *
+     * <p>
+     * We only find children that are one level down, i.e. if prefix is "yy.file" then we
+     * will find "yy.file.A", "yy.file.B", but not "yy.file.B.C".
+     * </p>
+     *
+     * @param properties   Properties to look in
+     * @param parentPrefix Parent property prefix to use as starting point
+     *
+     * @return List of children that match the passed-in parent prefix, always non-null.
+     */
+    public static List<String> getMatchingChildren(Properties properties, String parentPrefix) {
+        List<String> matches = new ArrayList<String>();
+
+        if (properties != null) {
+            for (Object key : properties.keySet()) {
+                String name = (String) key;
+                if (name.startsWith(parentPrefix + ".")) {
+                    String remaining = substring(name, (parentPrefix + ".").length());
+                    String match = parentPrefix + "." + remaining.replaceFirst("\\..*$", "");
+                    if (!matches.contains(match)) {
+                        matches.add(match);
+                    }
+                }
+            }
+        }
+
+        return matches;
     }
 
 }
