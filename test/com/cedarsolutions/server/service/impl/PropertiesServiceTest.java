b47846e74a5bf4efb13a1d3bea58dd4d72b36abb
Kenneth J. Pronovici <pronovic@ieee.org>
2015-11-17 16:15:23 -0600

    Add PropertiesService as a service wrapper over PropertiesUtils.

---
+++ test/com/cedarsolutions/server/service/impl/PropertiesServiceTest.java
@@ -0,0 +1,240 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *              C E D A R
+ *          S O L U T I O N S       "Software done right."
+ *           S O F T W A R E
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Copyright (c) 2014-2015 Kenneth J. Pronovici.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Apache License, Version 2.0.
+ * See LICENSE for more information about the licensing terms.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Author   : Kenneth J. Pronovici <pronovic@ieee.org>
+ * Language : Java 6
+ * Project  : Common Java Functionality
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package com.cedarsolutions.server.service.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.cedarsolutions.exception.InvalidDataException;
+import com.cedarsolutions.server.service.IPropertiesService;
+import com.cedarsolutions.util.FilesystemUtils;
+
+/**
+ * Unit test for PropertiesService, identical to the tests for PropertiesUtils.
+ * @author Kenneth J. Pronovici <pronovic@ieee.org>
+ */
+public class PropertiesServiceTest {
+
+    /** Create the build/tmp/PropertiesUtilsTest directory before running tests. */
+    @BeforeClass
+    public static void createTempDir() {
+        FilesystemUtils.createDir("build/tmp/PropertiesUtilsTest");
+    }
+
+    /** Remove the build/tmp/PropertiesUtilsTest directory before running tests. */
+    @AfterClass
+    public static void removeTempDir() {
+        FilesystemUtils.removeEmptyDir("build/tmp/PropertiesUtilsTest");
+    }
+
+    /** Test loadProperties() for valid properties. */
+    @Test public void testLoadPropertiesValid() throws Exception {
+        try {
+            FilesystemUtils.removeFile("build/tmp/PropertiesUtilsTest/valid.properties");
+
+            try {
+                getService().loadProperties("build/tmp/PropertiesUtilsTest/valid.properties");
+                fail("Expected InvalidDataException");
+            } catch (InvalidDataException e) { }
+
+            List<String> lines = new ArrayList<String>();
+            lines.add("instance.instanceName=instance");
+            FilesystemUtils.writeFileContents("build/tmp/PropertiesUtilsTest/valid.properties", lines);
+
+            Properties properties = getService().loadProperties("build/tmp/PropertiesUtilsTest/valid.properties");
+            assertNotNull(properties);
+            assertEquals("instance", properties.get("instance.instanceName"));  // just spot-check
+        } finally {
+            FilesystemUtils.removeFile("build/tmp/PropertiesUtilsTest/valid.properties");
+        }
+    }
+
+    /** Test loadProperties() for invalid properties. */
+    @Test public void testLoadPropertiesInvalid() throws Exception {
+        try {
+            FilesystemUtils.removeFile("build/tmp/PropertiesUtilsTest/invalid.properties");
+
+            List<String> lines = new ArrayList<String>();
+            lines.add("\\uZXXASA");  // illegal unicode escape sequence means properties can't be parsed
+            FilesystemUtils.writeFileContents("build/tmp/PropertiesUtilsTest/invalid.properties", lines);
+
+            try {
+                getService().loadProperties("build/tmp/PropertiesUtilsTest/invalid.properties");
+                fail("Expected InvalidDataException");
+            } catch (InvalidDataException e) { }
+        } finally {
+            FilesystemUtils.removeFile("build/tmp/PropertiesUtilsTest/invalid.properties");
+        }
+    }
+
+    /** Test getMatchingPrefixes() for null properties. */
+    public void testGetMatchingPrefixesNull() {
+        Properties properties = null;
+        List<String> matches = getService().getMatchingPrefixes(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for empty properties. */
+    public void testGetMatchingPrefixesEmpty() {
+        Properties properties = new Properties();
+        List<String> matches = getService().getMatchingPrefixes(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for properties where nothing matches the prefix. */
+    public void testGetMatchingPrefixesNoMatch() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingPrefixes(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for properties where non-parent matches the prefix. */
+    public void testGetMatchingPrefixesNonParent() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingPrefixes(properties, "b");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingPrefixes() for properties where a single parent matches the prefix. */
+    public void testGetMatchingPrefixesSingleParent() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingPrefixes(properties, "c");
+        assertFalse(matches.isEmpty());
+        assertEquals(1, matches.size());
+        assertEquals("c.one", matches.get(0));
+    }
+
+    /** Test getMatchingPrefixes() for properties where a multiple parents match the prefix. */
+    public void testGetMatchingPrefixesMultipleParents() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingPrefixes(properties, "a");
+        assertFalse(matches.isEmpty());
+        assertEquals(2, matches.size());
+        assertTrue(matches.contains("a.two"));
+        assertTrue(matches.contains("a.three"));
+    }
+
+    /** Test getMatchingPrefixes() for properties where parents and non-parents match the prefix. */
+    public void testGetMatchingPrefixesVarious() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingPrefixes(properties, "starts.with.d");
+        assertFalse(matches.isEmpty());
+        assertEquals(2, matches.size());
+        assertTrue(matches.contains("starts.with.d.two"));
+        assertTrue(matches.contains("starts.with.d.three"));
+    }
+
+    /** Test getMatchingChildren() for null properties. */
+    public void testGetMatchingChildrenNull() {
+        Properties properties = null;
+        List<String> matches = getService().getMatchingChildren(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingChildren() for empty properties. */
+    public void testGetMatchingChildrenEmpty() {
+        Properties properties = new Properties();
+        List<String> matches = getService().getMatchingChildren(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingChildren() for properties where nothing matches the prefix. */
+    public void testGetMatchingChildrenNoMatch() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingChildren(properties, "prefix");
+        assertTrue(matches.isEmpty());
+    }
+
+    /** Test getMatchingChildren() for properties where non-parent matches the prefix. */
+    public void testGetMatchingChildrenNonParent() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingChildren(properties, "b");
+        assertFalse(matches.isEmpty());
+        assertEquals(1, matches.size());
+        assertEquals("b.one", matches.get(0));
+    }
+
+    /** Test getMatchingChildren() for properties where a single parent matches the prefix. */
+    public void testGetMatchingChildrenSingleParent() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingChildren(properties, "c");
+        assertFalse(matches.isEmpty());
+        assertEquals(1, matches.size());
+        assertEquals("c.one", matches.get(0));
+    }
+
+    /** Test getMatchingChildren() for properties where a multiple parents match the prefix. */
+    public void testGetMatchingChildrenMultipleParents() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingChildren(properties, "a");
+        assertFalse(matches.isEmpty());
+        assertEquals(2, matches.size());
+        assertTrue(matches.contains("a.two"));
+        assertTrue(matches.contains("a.three"));
+    }
+
+    /** Test getMatchingChildren() for properties where parents and non-parents match the prefix. */
+    public void testGetMatchingChildrenVarious() {
+        Properties properties = createProperties();
+        List<String> matches = getService().getMatchingChildren(properties, "starts.with.d");
+        assertFalse(matches.isEmpty());
+        assertEquals(3, matches.size());
+        assertTrue(matches.contains("starts.with.d.one"));
+        assertTrue(matches.contains("starts.with.d.two"));
+        assertTrue(matches.contains("starts.with.d.three"));
+    }
+
+    /** Create properties for testing. */
+    private static Properties createProperties() {
+        Properties properties = new Properties();
+
+        properties.put("a.two.1", "a21");
+        properties.put("a.two.2", "a21");
+        properties.put("a.three.1", "a31");
+        properties.put("b.one", "b1");
+        properties.put("c.one.1", "c11");
+        properties.put("starts.with.d.one", "d1");
+        properties.put("starts.with.d.two.1", "d21");
+        properties.put("starts.with.d.two.2", "d21");
+        properties.put("starts.with.d.three.1", "d31");
+
+        return properties;
+    }
+
+    /** Get a service instance for testing. */
+    private static IPropertiesService getService() {
+        return new PropertiesService();
+    }
+
+}
---
+++ util/com/cedarsolutions/server/service/IPropertiesService.java
@@ -0,0 +1,75 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *              C E D A R
+ *          S O L U T I O N S       "Software done right."
+ *           S O F T W A R E
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Copyright (c) 2014-2015 Kenneth J. Pronovici.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Apache License, Version 2.0.
+ * See LICENSE for more information about the licensing terms.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Author   : Kenneth J. Pronovici <pronovic@ieee.org>
+ * Language : Java 6
+ * Project  : Common Java Functionality
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package com.cedarsolutions.server.service;
+
+import java.util.List;
+import java.util.Properties;
+
+import com.cedarsolutions.exception.InvalidDataException;
+
+/**
+ * Service wrapper around PropertiesUtils.
+ * @author Kenneth J. Pronovici <pronovic@ieee.org>
+ */
+public interface IPropertiesService {
+
+    /**
+     * Load Java properties from disk.
+     * @param paths   Paths of the Java properties files to process, in the desired order
+     * @return Properties object filled in with all properties from the passed-in files.
+     * @throws InvalidDataException  If properties could not be loaded.
+     */
+    Properties loadProperties(String... paths) throws InvalidDataException;
+
+    /**
+     * Get a list of existing prefixes which match the passed-in prefix one level down.
+     *
+     * <p>
+     * For instance, if prefix is "database" and the properties object contains properties
+     * like "database.production.x.y" and "database.integration.x.y.", the returned list
+     * will contain "database.production" and "database.integration".
+     * </p>
+     *
+     * @param properties   Properties to look in
+     * @param parentPrefix Parent property prefix to use as starting point
+     *
+     * @return List of prefixes that match the passed-in prefix, always non-null.
+     */
+    List<String> getMatchingPrefixes(Properties properties, String parentPrefix);
+
+    /**
+     * Get a list of children which have a given parent prefix.
+     *
+     * <p>
+     * We only find children that are one level down, i.e. if prefix is "yy.file" then we
+     * will find "yy.file.A", "yy.file.B", but not "yy.file.B.C".
+     * </p>
+     *
+     * @param properties   Properties to look in
+     * @param parentPrefix Parent property prefix to use as starting point
+     *
+     * @return List of children that match the passed-in parent prefix, always non-null.
+     */
+    List<String> getMatchingChildren(Properties properties, String parentPrefix);
+
+}
